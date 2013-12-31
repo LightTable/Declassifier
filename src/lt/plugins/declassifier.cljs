5f3632975385d088b8d02c551754f059106493f9
Jamie Brandon <jamie@scattered-thoughts.net>
2013-12-31 02:25:54 +0000

    Decompose into commands

--- src/lt/plugins/declassifier.cljs
+++ src/lt/plugins/declassifier.cljs
@@ -1,6 +1,7 @@
 (ns lt.plugins.declassifier
   (:require [lt.objs.editor :as editor]
-            [lt.objs.editor.pool :as pool])
+            [lt.objs.editor.pool :as pool]
+            [lt.objs.command :as cmd])
   (:require-macros [lt.macros :refer [behavior]]))
 
 (def classy->plain
@@ -13,7 +14,7 @@
    string
    classy->plain))
 
-(defn declassify-near-cursor []
+(defn declassify-behind-cursor []
   (let [cm (editor/->cm-ed (pool/last-active))
         cursor (.getCursor cm)
         from #js {:line (.-line cursor) :ch 0}
@@ -22,9 +23,20 @@
     (when (not= plain-range classy-range)
       (.replaceRange cm plain-range from cursor))))
 
-(behavior ::on-change
-          :triggers #{:change}
-          :desc "Declassifier: Trigger on change"
-          :debounce 500
-          :reaction (fn [this]
-                      (declassify-near-cursor)))
+(cmd/command {:command :declassify-behind-cursor
+              :desc "Declassifier: replace classy words behind the cursor"
+              :exec (fn []
+                      (declassify-behind-cursor))})
+
+(defn declassify-editor []
+  (let [cm (editor/->cm-ed (pool/last-active))
+        cursor (.getCursor cm)]
+    (doseq [[classy plain] classy->plain]
+      (js/CodeMirror.commands.find cm classy false)
+      (js/CodeMirror.commands.replace cm plain false true))
+    (.setCursor cm cursor)))
+
+(cmd/command {:command :declassify-editor
+              :desc "Declassifier: replace classy words in the current editor"
+              :exec (fn []
+                      (declassify-editor))})
