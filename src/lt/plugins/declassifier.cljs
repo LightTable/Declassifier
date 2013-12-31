5171a39e0f43905d4a8679f0d61f8cd8f0f24e63
Jamie Brandon <jamie@scattered-thoughts.net>
2013-12-31 01:31:58 +0000

    Declassify

--- src/lt/plugins/declassifier.cljs
+++ src/lt/plugins/declassifier.cljs
@@ -1,9 +1,30 @@
 (ns lt.plugins.declassifier
+  (:require [lt.objs.editor :as editor]
+            [lt.objs.editor.pool :as pool])
   (:require-macros [lt.macros :refer [behavior]]))
+
+(def classy->plain
+  {"behaviour" "behavior"})
+
+(defn declassify-string [string]
+  (reduce
+   (fn [string [classy plain]]
+     (.replace string classy plain))
+   string
+   classy->plain))
+
+(defn declassify-near-cursor []
+  (let [cm (editor/->cm-ed (pool/last-active))
+      cursor (.getCursor cm)
+      from #js {:line (- (.-line cursor) 10)}
+      to #js {:line (+ (.-line cursor) 10)}
+      classy-range (.getRange cm from to)
+      plain-range (declassify-string classy-range)]
+  (.replaceRange cm plain-range from to)))
 
 (behavior ::on-change
           :triggers #{:change}
           :desc "Declassifier: Trigger on change"
           :debounce 100
           :reaction (fn [this]
-                      (js/alert "Class detected!")))
+                      (declassify-near-cursor)))
